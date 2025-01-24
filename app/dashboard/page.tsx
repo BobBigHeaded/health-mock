27c9ee592eb3c3d83fe223676fbe4ca4a1f430f1
BobBigHeaded <kieran.hodgson86@gmail.com>
2025-01-24 15:44:02 +0000

    supabase connection

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,31 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
-
 export default async function Dashboard() {
-  const supabaseUrl = "https://suajjpdsxhqkpzubqjam.supabase.co";
-  const supabase = await createClient(
-    supabaseUrl,
-    `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-    eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN1YWp
-    qcGRzeGhxa3B6dWJxamFtIiwicm9sZSI6ImFub2
-    4iLCJpYXQiOjE3MzI4MDEzNDgsImV4cCI6MjA0O
-    DM3NzM0OH0.xx4YVAqoYNEojS8VC7PXssqrY458
-    NSDBryMwF-GQLIg`
-  );
-  const { data: conditions } = await supabase.from("conditions").select("*");
-  return <pre>{JSON.stringify(conditions, null, 2)}</pre>;
 
-  return (
-    <div className="flex flex-1 flex-row px-[30px] gap-[60px]">
-      <div className="flex flex-1 justify-center items-center">
-        <div className="flex min-h-[400px] w-full justify-center items-center bg-white rounded-full">
-          djaidow
-        </div>
-      </div>
-      <div className="flex justify-center items-center flex-1">
-        <div className="flex justify-center items-center h-[800px] w-full bg-white rounded-[60px] border border-[#0A5F94] border-[5px]">
-          <div></div>
-        </div>
-      </div>
-    </div>
-  );
+  return <p>wdad</p>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import  {createClient} from '@/utils/supabase/server'
+
 export default async function Home() {
+
+    const  supabase = await createClient();
+    const { data: id } = await supabase.rpc('get_trigger_id', {temp: 50, hum: 60, pol: 50});
+
   return (
     <div>
-      <p>Freaky</p>
+      <pre>{JSON.stringify(id, null, 2)}</pre>
     </div>
   );
 }
